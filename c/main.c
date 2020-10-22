0de3cffaafb30596890df791f7644b5a5257ec09
johnsavadkuhi <physitime@gmail.com>
2020-10-22 20:52:11 +0330

    create main.c file

---
+++ c/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+#include <string.h> 
+
+void add () {
+    int x = 4 ; 
+}
+
+
+// int main() {
+
+// int (* fp ) (int , int ) ; 
+
+// fp = add ; 
+
+// printf("%d", fp(2 ,3 )) ; 
+
+ 
+//  fp =sub ; 
+
+// printf( "%d", fp(5 , 2 )) ; 
+
+
+// }
+
