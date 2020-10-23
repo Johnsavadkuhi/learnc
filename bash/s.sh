108d5edc47b89831258c78bae307db9d0a68361a
johnsavadkuhi <physitime@gmail.com>
2020-10-24 02:27:02 +0330

    update

--- bash/s.sh
+++ bash/s.sh
@@ -80,26 +80,25 @@ test_string(){
 
 # }
 
-INT=10 
+int=-5 
 
-if [ -z "$INT" ];then 
-    echo "INT si empty. " >&2
-    exit 
-fi 
-
-if [ $INT -eq 0 ]; then 
-    echo "INT is zero" 
-
-else 
-
-    if [ $INT -lt 0  ]; then 
-        echo "INT is gegative "
+if [[ "$int" =~ ^-?[0-9]+$ ]]; then 
+    if [ $int -eq 0 ];then 
+        echo "INT is zero" 
     else 
-        echo "INT is positive. "
-    fi 
-    if [ $((INT%2)) -eq 0 ]; then 
-        echo "INT is even"
+        if [ $int -lt 0 ] ; then 
+            echo "INT is negative" 
+        else 
+            echo "Int is positive"
+        fi 
+        if [ $((int % 2 )) -eq 0 ];then 
+            echo "INt is even"
+        else 
+            echo "INT is odd"
+        fi 
+    fi
     else 
-        echo "INT is odd" 
-    fi 
+        echo "INT is not an integer " >&2 
+        exit 1  
 fi 
+ 
