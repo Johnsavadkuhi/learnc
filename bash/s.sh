ef0f5dec20f2d01b5a076179565af6a669c8ca47
johnsavadkuhi <physitime@gmail.com>
2020-10-23 19:01:00 +0330

    create test_file method

--- bash/s.sh
+++ bash/s.sh
@@ -1,3 +1,16 @@
 
 #!/bin/bash
 
+test_file(){
+    
+    FILE=~/.bashrc 
+
+    if [ -e "$FILE" ] ; then 
+        if [ -f "$FILE" ];then 
+            echo "$FILE is reqular file " 
+        fi
+    fi     
+}
+
+
+test_file 
