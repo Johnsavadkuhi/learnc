d08bc26df917902486e2bb60fc49b5449e18de0f
johnsavadkuhi <physitime@gmail.com>
2020-10-23 19:22:51 +0330

    add test_string method

--- bash/s.sh
+++ bash/s.sh
@@ -3,14 +3,54 @@
 
 test_file(){
     
-    FILE=~/.bashrc 
+    FILE=~/.nvm 
 
     if [ -e "$FILE" ] ; then 
+        
+        echo "the $FILE is exist."
+
         if [ -f "$FILE" ];then 
             echo "$FILE is reqular file " 
         fi
+
+        if [ -d "$FILE" ]; then 
+            echo "$FILE is directory"
+        fi 
+
+        if [ -r "$FILE" ] ;then 
+            echo "$FILE is readable"
+        fi 
+
+        if [ -x "$FILE" ];then 
+            echo "$FILE is executable/searchable."
+        fi 
+
+    else 
+        echo "$FILE does not exist."
+        return 1 
+
     fi     
 }
 
 
-test_file 
+test_string(){
+
+    answer="no"
+
+    if [ -z "$answer" ]; then 
+        echo "There is no answer." >&2 
+        exit 1 
+    fi 
+
+    if [ "$answer" = "yes" ]; then 
+        echo "The answer is yes "
+    elif [ "$answer" = "no" ];then 
+        echo "the answer is no "
+    elif [ "$answer" = "maybe" ] ; then 
+        echo "the answer is maybe"
+    else 
+        echo "The answer is unkonw"
+    fi
+}
+
+test_string 
