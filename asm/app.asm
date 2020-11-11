c35091ae38a2da39df9d2f8cd488fa06f4af19c3
johnsavadkuhi <physitime@gmail.com>
2020-11-12 01:40:38 +0330

    update

--- asm/app.asm
+++ asm/app.asm
@@ -1,11 +1,55 @@
-section .text 
 
-global _start 
+section .data 
+
+    userMsg db 'Please enter a number : ' 
+    lenUserMsg equ $-userMsg 
+    dispMsg db 'You have entered : ' 
+    lenDispMsg equ $-dispMsg
+
+
+section .bss 
+
+    num resb 5  ; Unintialized data
+
+section .text 
+    
+    global _start 
 
 _start: 
 
-xor eax , eax 
-mov al , 1  
-mov ebx , ebx  
-int 0x80 
+    mov eax , 4 
+    mov ebx , 1 
+    mov ecx , userMsg 
+    mov edx , lenUserMsg
+    int 80h 
+
+; Read and store the user input 
+
+mov eax , 3 
+mov ebx , 2 
+mov ecx , num 
+mov edx , 5 
+int 80h 
+
+;Output the message 'The entered number is : ' 
+
+mov eax , 4 
+mov ebx , 1 
+mov ecx , dispMsg 
+mov edx , lenDispMsg 
+int 80h 
+
+; Output the number entered  
+
+mov eax , 4 
+mov ebx , 1 
+mov ecx , num 
+mov edx , 5 
+int 80h 
+
+;Exit code 
+
+mov eax , 1 
+mov ebx , 0
+int 80h  
 
